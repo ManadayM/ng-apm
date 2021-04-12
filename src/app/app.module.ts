0476092e49e9131e1bca4e4c20915f993b312154
Manaday Mavani <m.k.mavani@gmail.com>
2021-04-12 16:31:45 +0530

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
+import { StarComponent } from './shared/star.component';
+
+@NgModule({
+  // Our directives, components, and pipes go here
+  declarations: [
+    AppComponent,
+    ProductListComponent,
+    ConvertToSpacesPipe,
+    StarComponent
+  ],
+  // Other modules and third-party modules go here.
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/products/product-list.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ProductService } from './product.service';
+import { Product } from './shared/product.model';
+
+@Component({
+  selector: 'pm-products',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit, OnDestroy {
+
+  pageTitle = 'Product List';
+  imageWidth = 50;
+  imageMargin = 2;
+  showImage = false;
+
+  filteredProducts: Product[] = [];
+
+  private _listFilter = '';
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value: string) {
+    console.log('In setter: ', value);
+    this._listFilter = value;
+    this.filteredProducts = this.performFilter(value);
+  }
+
+  products: Product[] = [];
+  errorMessage: string = '';
+  sub!: Subscription;
+
+  constructor(private productService: ProductService) { }
+
+  ngOnInit(): void {
+    console.log('Product list initialized.');
+    // this.listFilter = 'cart';
+    this.sub = this.productService.getProducts().subscribe({
+      next: (products) => {
+        this.products = products;
+        this.filteredProducts = this.products;
+      },
+      error: err => this.errorMessage = err
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+
+  toggleImage(): void {
+    this.showImage = !this.showImage;
+  }
+
+  performFilter(_filterBy: string): Product[] {
+    const filterBy = _filterBy.toLowerCase();
+    return this.products.filter((product: Product) => {
+      return product.productName.toLowerCase().includes(filterBy);
+    })
+  }
+
+  onRatingClicked(message: string): void {
+    this.pageTitle = 'Product List: ' + message;
+  }
+}
